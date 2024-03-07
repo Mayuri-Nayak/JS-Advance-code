cbe2b50c0039af62aad3ecb4b41747831729448e
Mayuri-Nayak <mayurinayak2002@gmail.com>
2024-03-07 15:29:20 +0530

    first commit

---
+++ modules/library.js
@@ -0,0 +1,12 @@
+export let message = "E26 Modules";
+
+export function user(name){
+    console.log(`Hello ${name}`);
+}
+
+export class test{
+    constructor(){
+        console.log("This is Constructor.");
+    }
+}
+
---
+++ modules/main.js
@@ -0,0 +1,11 @@
+import { message , user , test } from "./library.js";
+//import * as Tanu from "./library.js";
+//console.log(Tanu.message);
+
+console.log (message);
+
+document.body.innerHTML = message;
+
+user("Tanushri");
+
+let a = new test();
